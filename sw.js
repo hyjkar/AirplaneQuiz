92af87fda372bff36964027b23ea78b4ae5d8cde
hyjkar <heikki.kareranta@gmail.com>
2026-02-12 14:26:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,3 @@
-// Minimal Service Worker to satisfy PWA installation requirements
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-});
-
 self.addEventListener('fetch', (e) => {
-  // Just pass requests through normally
   e.respondWith(fetch(e.request));
 });
